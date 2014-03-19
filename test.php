ca97022792ac24cd1d159dc86f070210ef2b29fe
maria <mariasorlie92@gmail.com>
2014-03-19 17:16:37 +0100

    fjern tull

--- test.php
+++ test.php
@@ -5,10 +5,5 @@ echo 'Martin';
 echo 'simen sug';
 echo 'maria sint!';
 echo 'Martin sug';
-<<<<<<< HEAD
-echo 'lolollooolollololol*';
-echo 'du suuuuug martin!!!';
-=======
-echo 'lolita!!!';
->>>>>>> 87e4bb8e7ebabf7f2c7d9fdb1c640d95b49caf11
+
 ?>
